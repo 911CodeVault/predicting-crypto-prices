ae3b3384862b0fc359632b152078fc2b3d3f802c
911CodeVault <urbanwars.app@gmail.com>
2021-04-18 08:22:50 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import pandas_datareader as web
+import datetime as dt 
+
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
+
+crypto_currency = 'BTC'
+# crypto_currency = 'DOGE'
+against_currency = 'USD'
+
+start = dt.datetime(2016,1,1)
+end = dt.datetime.now()
+
+data = web.DataReader(f'{crypto_currency}-{against_currency}','yahoo',start,end)
+
+
+# Prepare data in the neural network
+print(data.head())
